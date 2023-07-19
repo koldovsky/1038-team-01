56988999e9cd89cc59291f1fe94c4f49390ef216
Vyacheslav Koldovskyy <koldovsky@koldovsky.com>
2023-07-19 20:29:28 +0300

    Create tasks-01.js

---
+++ js-tasks/tasks-01.js
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/583710ccaa6717322c000105/train/javascript
+
+function simpleMultiplication(number) {
+    if (number % 2 === 1) {
+      return number * 9;
+    } else {
+      return number * 8;
+    }
+}
+
