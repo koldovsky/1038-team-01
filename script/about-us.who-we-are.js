fd08dca28da3d02ad55d776b6116d2739c556e31
DenysVerbitskyi <deniv.verbitskiy90@gmail.com>
2023-07-30 17:30:54 +0300

    Added api to index.news and about-us.who-we-are

--- script/about-us.who-we-are.js
+++ script/about-us.who-we-are.js
@@ -1,3 +1,8 @@
+const response = await fetch("../api/about-us.who-we-are.json");
+const articles = await response.json();
+
+renderWhoWeAre(articles);
+
 const buttons = document.querySelectorAll('.accordion__item-button');
 
 buttons.forEach(button => {
@@ -19,3 +24,26 @@ buttons.forEach(button => {
 
     })
 });
+
+function renderWhoWeAre(articles) {
+    const articlesContainer = document.querySelector('.about-us-who-we-are__accordion');
+    articlesContainer.innerHTML = "";
+    for (const article of articles) {
+        articlesContainer.innerHTML += `<div class="accordion__item item">
+        <div class="accordion__item-container">
+            <img class="item__image" src="${article.image}" alt="${article.alt}">
+            <div class="accordion__item-row-container">
+                <h3 class="item__header">${article.header}</h3>
+                <div class="accordion__item-button">
+                    <div class="accordion__item-button-line accordion__item-button-line1"></div>
+                    <div class="accordion__item-button-line accordion__item-button-line2" id="line2"></div>
+                </div>
+                <div class="accordion__item-content">
+                    <p>${article.content}</p>
+                </div>
+            </div>
+
+        </div>
+    </div>`
+    }
+}
--- script/news.js
+++ script/news.js
@@ -1,26 +1,5 @@
-const articles = [
-    {
-        image: 'img/index/news/color-book.png',
-        alt: 'Color book',
-        date: 'TIPS | 18.10.2022',
-        title: 'How Does Color Influence Our Decisions?',
-        content: 'The psychology of color is one of the most controversial and interesting marketing aspects, mainly because of the depth of analysis.'
-    },
-    {
-        image: 'img/index/news/designers-at-laptop.png',
-        alt: 'Designers working on laptop',
-        date: 'TIPS | 18.10.2022',
-        title: 'The Importance of Integrated Web Teams',
-        content: 'Your engineers can bring you better results! Learn how to improve the engineering department to make a new step for your company growth.'
-    },
-    {
-        image: 'img/index/news/designers-at-board.png',
-        alt: 'Two designers at a whiteboard',
-        date: 'TIPS | 18.10.2022',
-        title: 'On-Time and On-Budget Website Design',
-        content: 'When customers pay for a product, they want to be sure of the deadline, especially when the project is large and expensive.'
-    }
-]
+const response = await fetch("../api/index.news.json");
+const articles = await response.json();
 
 renderArticles(articles);
 
