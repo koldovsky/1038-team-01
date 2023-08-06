2b90fa0b0aaab7756c0f2ea44a9b368fb92af29f
ihorbroslavsky <58515005+ihorbroslavsky@users.noreply.github.com>
2023-08-06 20:58:05 +0000

    brands css updated

--- script/brands_carousel.js
+++ script/brands_carousel.js
@@ -1,10 +1,10 @@
 const brands = [
-    '<div class="brands__carousel-item"><img class="brands__carousel-item-image" src="img/index/brands/vue.svg" alt="VUE"></div>',
-    '<div class="brands__carousel-item"><img class="brands__carousel-item-image" src="img/index/brands/blink.svg" alt="Blink"></div>',
-    '<div class="brands__carousel-item"><img class="brands__carousel-item-image" src="img/index/brands/nike.svg" alt="Nike"></div>',
-    '<div class="brands__carousel-item"><img class="brands__carousel-item-image" src="img/index/brands/the-cw.svg" alt="The CW"></div>',
-    '<div class="brands__carousel-item"><img class="brands__carousel-item-image" src="img/index/brands/adidas.svg" alt="Adidas"></div>',
-    '<div class="brands__carousel-item"><img class="brands__carousel-item-image" src="img/index/brands/caractere.svg" alt="Caractere"></div>',
+    '<div class="brands__carousel-item"><div class="brands__carousel-item-image-container"><img class="brands__carousel-item-image" src="img/index/brands/vue.svg" alt="VUE"></div></div>',
+    '<div class="brands__carousel-item"><div class="brands__carousel-item-image-container"><img class="brands__carousel-item-image" src="img/index/brands/blink.svg" alt="Blink"></div></div>',
+    '<div class="brands__carousel-item"><div class="brands__carousel-item-image-container"><img class="brands__carousel-item-image" src="img/index/brands/nike.svg" alt="Nike"></div></div>',
+    '<div class="brands__carousel-item"><div class="brands__carousel-item-image-container"><img class="brands__carousel-item-image" src="img/index/brands/the-cw.svg" alt="The CW"></div></div>',
+    '<div class="brands__carousel-item"><div class="brands__carousel-item-image-container"><img class="brands__carousel-item-image" src="img/index/brands/adidas.svg" alt="Adidas"></div></div>',
+    '<div class="brands__carousel-item"><div class="brands__carousel-item-image-container"><img class="brands__carousel-item-image" src="img/index/brands/caractere.svg" alt="Caractere"></div></div>',
 ];
 
 
@@ -14,7 +14,7 @@ let currentSlide = 0;
 
 function renderSlide() {
     carouselSlide.innerHTML = brands[currentSlide];
-    if (window.innerWidth >= 767) {
+    if (window.innerWidth >= 768) {
         const secondSlideIdx = currentSlide + 1 >= brands.length ? 0 : currentSlide + 1;
         carouselSlide.innerHTML += brands[secondSlideIdx];
         const thirdSlideIdx = secondSlideIdx + 1 >= brands.length ? 0 : secondSlideIdx + 1;
